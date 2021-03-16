f8cb06716374d7e2d406a1137ed42e27cd0d7b26
Fabien-Pinna <fabien.pinna@hotmail.fr>
2021-03-16 19:32:58 +0100

    Work in progress on the home page

--- front/src/components/Nav/Nav.vue
+++ front/src/components/Nav/Nav.vue
@@ -11,4 +11,6 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./style.scss";
+</style>
---
+++ front/src/views/Home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img class="logo" alt="webinfab logo" src="@/assets/img/logo.jpg" />
+    <!-- <Header/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Header from "@/components/Header/Header";
+
+export default {
+  name: "Home",
+  // components: {
+  //   Header
+  // }
+};
+</script>
+
+<style lang="scss">
+@import "./style.scss";
+</style>
