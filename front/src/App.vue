b2a5e59806b10792f96039bf858045d8fe5e2f8a
Fabien-Pinna <fabien.pinna@hotmail.fr>
2021-03-16 16:02:44 +0100

    First steps

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Nav />
   <router-view />
 </template>
 
+<script>
+import Nav from "./components/Nav/Nav";
+export default {
+  name: "App",
+  components: {
+    Nav
+  }
+}
+</script>
+
 <style lang="scss">
 @import "./assets/styles/reset.scss";
-
 </style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="webinfab logo" src="../assets/img/logo.jpg" />
+    <Header/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header/Header";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Header
   }
 };
 </script>
